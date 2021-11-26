e096d314833c21161057e8698b1dea830a7f9432
cnqinglin <2207328000@qq.com>
2021-11-26 20:20:40 +0800

    成功连接数据库

--- src/index.ts
+++ src/index.ts
@@ -2,6 +2,6 @@ import "reflect-metadata";
 import {createConnection} from "typeorm";
 
 createConnection().then(async connection => {
-    console.log('connection', connection)
-    connection.close()
+  console.log(connection);
+  connection.close()
 }).catch(error => console.log(error));
