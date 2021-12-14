0c890b1fae5020e040cf7aa823afb1c79b3b1b3f
cnqinglin <2207328000@qq.com>
2021-12-14 20:55:39 +0800

    改正表名

--- pages/api/v1/posts.tsx
+++ pages/api/v1/posts.tsx
@@ -1,27 +1,28 @@
-import { getDatabaseConnection } from "lib/getDataBaseConnection"
-import { withSession } from "lib/withSession"
-import { NextApiHandler, NextApiRequest, NextApiResponse } from "next"
-import { Post } from "src/entity/Post"
+import {NextApiHandler} from 'next';
+import {Post} from '../../../src/entity/Post';
+import {withSession} from '../../../lib/withSession';
+import { getDatabaseConnection } from 'lib/getDataBaseConnection';
+
+
 const Posts: NextApiHandler = withSession(async (req, res) => {
-    // markdown 形式获取文章列表
-    // const posts = await getPosts()
-    // res.statusCode = 200;
-    // res.setHeader('Content-Type', 'application/json,charset=utf-8');
-    // res.write(JSON.stringify(posts));
-    // res.end()
-
-    // 获取数据库中的信息
-    if (req.method === 'POST') {
-        const user = req.session.get('currentUser');
-        console.log('11111111111-1',user);
-        const posts = new Post()
-        const connection = await getDatabaseConnection()
-        posts.author = user;
-        connection.manager.save(posts)
-        console.log('11111111111');
-        res.json(posts)
-        console.log('11111111111-2');
-    }
+  if (req.method === 'POST') {
+    console.log('1111111111');
+    const {title, content} = req.body;
+    console.log('22222222');
+    const post = new Post();
+    console.log('3333333333333333');
+    post.title = title;
+    console.log('4444444444444');
+    post.content = content;
+    console.log('55555555555555');
+    const user = req.session.get('currentUser');
+    console.log('user');
+    console.log(user);
+    post.author = user;
+    const connection = await getDatabaseConnection();
+    console.log('66666666666666666');
+    await connection.manager.save(post);
+    res.json(post);
+  }
 });
-
-export default Posts
+export default Posts;
