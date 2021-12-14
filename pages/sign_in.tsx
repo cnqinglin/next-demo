1881a07b93335ed52cd15ec8b1ad7dca3abc478e
cnqinglin <2207328000@qq.com>
2021-12-14 11:30:50 +0800

    登陆注册页面也使用useForm

--- pages/sign_in.tsx
+++ pages/sign_in.tsx
@@ -4,25 +4,11 @@ import axios, { AxiosError, AxiosResponse } from 'axios';
 import { withSession } from 'lib/withSession';
 import { User } from 'src/entity/User';
 import { Form } from 'components/Form';
+import { useForm } from 'hooks/useForm';
 
 const SignIn: NextPage<{ user: User }> = (props) => {
-  const [formData, setFormData] = useState({
-    username: '',
-    password: '',
-    // passwordConfirmation: ''
-  });
-  const [errors, setErrors] = useState({
-    username: [], password: [],
-    // passwordConfirmation: []
-  });
-  const onChange = useCallback((key, value) =>
-    setFormData({
-      ...formData,
-      [key] : value
-    })
-    , [formData]);
-  const onSubmit = useCallback((e) => {
-    e.preventDefault();
+  const initFormData = { username: '',password: '',}
+  const onSubmit = (formData:typeof initFormData) => {
     axios.post(`/api/v1/sessions`, formData)
       .then(() => {
         window.alert('登录成功！')
@@ -41,31 +27,29 @@ const SignIn: NextPage<{ user: User }> = (props) => {
       }).catch(res => {
         console.log('resresres', res)
       });
-  }, [formData]);
+  }
+  const { form, setErrors } = useForm({
+    initFormData, 
+    onSubmit,
+    fields: [
+      {
+        label: '用户名:',
+        type: 'text',
+        key:'username',
+      }, {
+        label: '密码：',
+        type: 'password',
+        key:'password',
+      }
+    ],
+    buttons:<button type="submit">登录</button>
+  })
+  
   return (
     <>
       {props.user && <div>当前登录用户为{props.user.username}</div>}
       <h1>登录</h1>
-      <Form
-        fields={[
-          {
-            label: '用户名:',
-            type: 'text',
-            value:formData.username,
-            onChange: e => onChange('username',e.target.value),
-            errors: errors.username
-          }, {
-            label: '密码：',
-            type: 'password',
-            value:formData.password,
-            onChange: e => onChange('password',e.target.value),
-            errors: errors.password,
-          }
-        ]}
-        onSubmit={onSubmit}
-        buttons={
-          <><button type="submit">登录</button>
-        </>}/>
+      { form }
     </>
   );
 };
